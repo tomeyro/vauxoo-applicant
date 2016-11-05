f80dd1489c035904c472656118fba19680d715f4
Tomas Alvarez <tomalvarezf@gmail.com>
2016-11-04 20:18:06 -0400

    Create hobbies table and add hobbies to employees

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -10,7 +10,7 @@ CREATE TABLE employee_department (
   description varchar(255)
 );
 
--- Creating staff table
+-- Creating employees table
 CREATE TABLE employee (
   id integer PRIMARY KEY,
   first_name varchar(255),
@@ -20,13 +20,37 @@ CREATE TABLE employee (
 
 -- Populating departments table
 INSERT INTO employee_department (id, name, description) VALUES
-(1, 'Production', 'Production Department'), (2, 'Accounting', 'Accounting Department'),
-(3, 'Purchasing', 'Purchasing Department'), (4, 'Marketing', 'Marketing Department'),
-(5, 'HR', 'HR Department'), (6, 'IT', 'IT Department');
+(1, 'Production', 'Production department'), (2, 'Accounting', 'Accounting department'),
+(3, 'Purchasing', 'Purchasing department'), (4, 'Marketing', 'Marketing department'),
+(5, 'HR', 'HR department'), (6, 'IT', 'IT department');
 
--- Populating staff table
+-- Populating employees table
 INSERT INTO employee (id, first_name, last_name, department_id) VALUES
 (1, 'Tomas', 'Alvarez', 6), (2, 'Alicia', 'Fuenmayor', 2),
 (3, 'Zadquiel', 'Barros', 4), (4, 'Michelle', 'Barros', 5);
+
+-- Creating hobbies table
+CREATE TABLE employee_hobby (
+  id integer PRIMARY KEY,
+  name varchar(255),
+  description varchar(255)
+);
+
+-- Creating employees and hobbies relationship table
+CREATE TABLE employees_hobbies (
+  employee_id integer REFERENCES employee (id),
+  department_id integer REFERENCES employee_department (id),
+  PRIMARY KEY (employee_id, department_id)
+);
+
+-- Populating hobbies table
+INSERT INTO employee_hobby (id, name, description) VALUES
+(1, 'Soccer', 'Playing soccer'), (2, 'Video Games', 'Playing video games'),
+(3, 'Reading', 'Reading good books');
+
+-- Adding hobbies to employees
+INSERT INTO employees_hobbies (employee_id, department_id) VALUES
+(1, 2), (1, 3), (2, 3), (2, 1),
+(3, 1), (3, 2), (4, 1), (4, 3);
 
 -- ...
