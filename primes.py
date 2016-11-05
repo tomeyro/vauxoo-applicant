0789dda784c77aa5142a34ea2b2ff0dd049962b2
Tomas Alvarez <tomalvarezf@gmail.com>
2016-11-04 20:47:19 -0400

    Create class for prime number detection

--- primes.py
+++ primes.py
@@ -1,16 +1,21 @@
 """
-Your module documentation here
+Module for prime number detection
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    An instance of this class will be able to tell if an integer is a prime number
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Returns if a given integer is a prime number
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if not isinstance(num_int, (int, long)) or num_int <= 1:
+            return False
+        else:
+            for i in range(2, num_int):
+                if num_int % i == 0:
+                    return False
+            return True
